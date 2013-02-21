b0f0b195c0e7eac995f7cf0c4978f7e90f5deabe
Jesse Pollak <jpollak92@gmail.com>
2013-02-20 21:43:50 -0800

    adds jump to top button

--- public/j/app.js
+++ public/j/app.js
@@ -1,1 +1,20 @@
+var jumpShown = false,
+    showOffset = $('header').height(),
+    $jump = $('#jump-top-container');
 $('#home h1').slabText({});
+$('a').smoothScroll();
+$(window).on('scroll', function(e) {
+    var offset = $(this).scrollTop();
+    console.log(offset);
+    if(jumpShown) {
+        if (offset < showOffset) {
+            $jump.hide();
+            jumpShown = false;
+        }
+    } else {
+        if (offset > showOffset) {
+            $jump.show();
+            jumpShown = true;
+        }
+    }
+})
