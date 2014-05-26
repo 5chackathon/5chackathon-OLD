9adf5a955dba3be2c539039c88732f3241bd0024
Andy Russell <arussell123@gmail.com>
2014-05-26 12:15:05 -0700

    activate jump script when document is loaded

--- public/j/app.js
+++ public/j/app.js
@@ -1,19 +1,21 @@
-var jumpShown = false,
-    showOffset = $('header').height(),
-    $jump = $('#jump-top-container');
-$('.slab').slabText({});
-$('a').smoothScroll();
-$(window).on('scroll', function(e) {
-    var offset = $(this).scrollTop();
-    if(jumpShown) {
-        if (offset < showOffset) {
-            $jump.hide();
-            jumpShown = false;
+$(document).ready(function() {
+    var jumpShown = false;
+    var showOffset = $('header').height();
+    var $jump = $('#jump-top-container');
+    $('.slab').slabText({});
+    $('a').smoothScroll();
+    $(window).on('scroll', function(e) {
+        var offset = $(this).scrollTop();
+        if(jumpShown) {
+            if (offset < showOffset) {
+                $jump.hide();
+                jumpShown = false;
+            }
+        } else {
+            if (offset > showOffset) {
+                $jump.show();
+                jumpShown = true;
+            }
         }
-    } else {
-        if (offset > showOffset) {
-            $jump.show();
-            jumpShown = true;
-        }
-    }
-})
+    });
+});
